46248d957aa409004bcc75b690774cafe5b889d6
sdarmonski <s.darmonski@uvionix.com>
2022-05-31 14:22:07 +0300

    Changed FPS specification

--- camera-start-recording.sh
+++ camera-start-recording.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Get the FPS parameters
-fps_in=$1
-fps_out=$2
+# Get the FPS parameter
+fps=$1
 
 # Get the resolution parameter
-res=$3
+res=$2
 
 # Get the capture device parameter
-dev=$4
+dev=$3
 
 # Get a username
 usr=$(getent passwd | awk -F: "{if (\$3 >= $(awk '/^UID_MIN/ {print $2}' /etc/login.defs) && \$3 <= $(awk '/^UID_MAX/ {print $2}' /etc/login.defs)) print \$1}" | head -1)
@@ -41,8 +40,7 @@ do
     done
 
     # Start recording
-    # /usr/bin/ffmpeg -f v4l2 -r $fps -s $res -input_format mjpeg -i $dev -t 180 /home/$usr/Videos/$nowDate/$filename -async 1 -vsync 1 -y
-    /usr/bin/ffmpeg -f v4l2 -r $fps_in -s $res -input_format mjpeg -i $dev -r $fps_out -t 180 /home/$usr/Videos/$nowDate/$filename -vsync 1 -y
+    /usr/bin/ffmpeg -f v4l2 -s $res -input_format mjpeg -i $dev -r $fps -t 180 /home/$usr/Videos/$nowDate/$filename -vsync 1 -y
     chown $usr /home/$usr/Videos/$nowDate/$filename
     echo "Recording ended. Starting new file..."
 done
